6a5ada64179e92b1c264bf7fe1ffff372ed1d227
Ezequiel Archilla <earchilla@cefas.com.ar>
2022-09-23 17:27:58 -0300

    cambios

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
+  lat:number
+  lon:number
+  total:string
 
-  constructor() { }
+  constructor(public geolocation:Geolocation) {
+    this.getGeolocation()
+  }
 
   ngOnInit() {
   }
 
+  getGeolocation(){
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data:Geoposition) => {
+      this.lat = data.coords.latitude;
+      this.lon = data.coords.longitude;
+    });
+    // this.geolocation.getCurrentPosition().then((geoposition: Geoposition)=>{
+    //   this.lat = geoposition.coords.latitude;
+    //   this.lon = geoposition.coords.longitude;
+    // });
+  }
+
+  calculateDistance(lon1, lon2, lat1, lat2){
+    let p = 0.017453292519943295;
+    let c = Math.cos;
+    let a = 0.5 - c((lat1-lat2) * p) / 2 + c(lat2 * p) *c((lat1) * p) * (1 - c(((lon1- lon2) * p))) / 2;
+    let dis = (12742 * Math.asin(Math.sqrt(a)));
+    return Math.trunc(dis);
+}
 }
